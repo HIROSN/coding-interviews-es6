1859517625f2838fb951858cd470dc7153165d41
Hiroshi Nagata <hirosn@live.com>
2014-12-23 00:39:04 -0800

    Add zip.js

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ File | Category | Notes
 [breathfirst.js](lib/breathfirst.js) | Tree | Breath-first traversal
 [fibonacci.js](lib/fibonacci.js) | DP | Fibonacci number
 [palindrome.js](lib/palindrome.js) | Algorithm | Check if a linked list is palindrome
+[zip.js](lib/zip.js) | FP | Implement Array.zip
---
+++ test/zip_test.js
@@ -0,0 +1,57 @@
+'use strict';
+
+import { MyArray } from '../lib/zip';
+let expect = require('chai').expect;
+
+describe('Array.zip tests', () => {
+  let combiner = (bookmark, boxart) => {
+    return {
+      time: bookmark.time,
+      boxart: boxart.url
+    };
+  };
+
+  let tests = function *() {
+    yield {
+      bookmarks: [
+        {
+          videoId: 65432445,
+          time: 32432
+        }
+      ],
+      boxarts: [
+        {
+          videoId: 65432445,
+          width: 130,
+          height:200,
+          url:'http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg'
+        }
+      ],
+      combiner,
+      result: '[{"time":32432,' +
+        '"boxart":"http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg"}]'
+    };
+
+    yield {
+      bookmarks: [
+        {
+          videoId: 65432445,
+          time: 32432
+        }
+      ],
+      boxarts: [],
+      combiner,
+      result: '[]'
+    };
+  };
+
+  it('should return correct result', () => {
+    for (let test of tests()) {
+      expect(JSON.stringify(new MyArray().zip(
+        test.combiner,
+        test.bookmarks,
+        test.boxarts)))
+      .to.equal(test.result);
+    }
+  });
+});
