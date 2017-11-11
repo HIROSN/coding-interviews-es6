cb20fb9189e6629a40753bbd1289bb896df42b21
Hiroshi Nagata <hirosn@live.com>
2017-11-11 13:11:01 -0800

    disable mocha chai tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,6 @@
 
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-traceur-simple');
-  grunt.loadNpmTasks('grunt-simple-mocha');
 
   grunt.initConfig({
     jshint: {
@@ -17,44 +13,8 @@ module.exports = function(grunt) {
       options: {
         jshintrc: true
       }
-    },
-
-    clean: {
-      src: ['build/']
-    },
-
-    copy: {
-      readme: {
-        cwd: 'test/',
-        expand: true,
-        src: ['README.md'],
-        dest: './'
-      }
-    },
-
-    traceur: {
-      options: {
-        includeRuntime: true,
-        traceurOptions: '--experimental'
-      },
-
-      build: {
-        files: {
-          'build/all_tests.js': ['lib/*.js', 'test/*.js']
-        }
-      }
-    },
-
-    simplemocha: {
-      src: ['build/**/*.js']
     }
   });
 
-  grunt.registerTask('default', [
-    'jshint',
-    'clean',
-    'copy',
-    'traceur',
-    'simplemocha'
-  ]);
+  grunt.registerTask('default', ['jshint']);
 };
